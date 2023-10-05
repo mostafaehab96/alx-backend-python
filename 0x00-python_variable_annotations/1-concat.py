71e58f4b337055bac53f873a3dde282cd8b27235
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-05 15:37:07 +0300

    Add task 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines concat function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns concatenation of str1 and str2
+    """
+    return str1 + str2
