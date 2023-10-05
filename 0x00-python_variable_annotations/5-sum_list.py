7f269626c1844b063e2f7fb00fdfe2a7c3c0f897
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-05 17:44:18 +0300

    Add task  5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines sum_list function"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of input_list."""
+    return sum(input_list)
