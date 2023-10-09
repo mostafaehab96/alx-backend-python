cf9e1c264bbc9186bcfcfd7a717c20c2e7b631d7
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-10 00:25:39 +0300

    Add task  0x01-python_async_function, README and first task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Waiting for a random time"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """Waits for a random number to execute and return it"""
+    waiting_time = random.uniform(0, max_delay)
+    await asyncio.sleep(waiting_time)
+    return waiting_time
