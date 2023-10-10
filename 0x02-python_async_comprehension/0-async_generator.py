bfafaa5eb1a50adccd046a4b596537a20c88d59a
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-11 01:10:28 +0300

    Add typing annotaion

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """A synchronous random generator"""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Creates a 10 random numbers generator"""
     for i in range(10):
         await asyncio.sleep(1)
