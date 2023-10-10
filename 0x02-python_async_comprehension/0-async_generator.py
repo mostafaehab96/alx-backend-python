e4158e74d1946c7967b2e771f83218485c6a9336
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-11 01:04:07 +0300

    Add project  0x02-python_async_comprehension, README and first task

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""A synchronous random generator"""
+import random
+import asyncio
+
+
+async def async_generator():
+    """Creates a 10 random numbers generator"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
+
