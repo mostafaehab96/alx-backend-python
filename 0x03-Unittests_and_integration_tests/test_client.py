5806a548e15270e355d54028b5a6b33d805d25c9
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-31 01:30:16 +0200

    Add task 2 in test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,3 +23,14 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get.return_value.json.return_value = {"org": org}
             client_org = GithubOrgClient(org)
             self.assertEqual(client_org.org, {"org": org})
+
+    @parameterized.expand([
+        ("google", "google/repos"),
+        ("abc", "error")
+    ])
+    def test_public_repos_url(self, org, url):
+        """Testing public repos url"""
+        with patch("requests.get") as mock_get:
+            mock_get.return_value.json.return_value = {"repos_url": url}
+            client_org = GithubOrgClient(org)
+            self.assertEqual(client_org._public_repos_url, url)
